4cf3c1a2f350d1dfb3e70038479a798e68a0fefe
atsogt <atsogt17@gmail.com>
2025-01-06 14:34:50 -0500

    pizza factory explained

--- ch4_factory_pattern/PizzaStore.java
+++ ch4_factory_pattern/PizzaStore.java
@@ -1,17 +1,21 @@
+package ch4_factory_pattern;
 
 public class PizzaStore {
+	// reference to factory
 	SimplePizzaFactory factory;
 
-	// client of pizza factory
+	// client of pizza factory, passing factory in through constructor
+	// also there is a opportunity to create different factories in the future...
 	public PizzaStore(SimplePizzaFactory factory) {
 		this.factory = factory;
 	}
 
 	public Pizza orderPizza(String type) {
 		Pizza pizza;
-
+		// we now have access to createPizza method from factory
+		// we also replace the 'new' concrete instantiation with a clean method call
 		pizza = factory.createPizza(type);
-
+		//
 		pizza.prepare();
 		pizza.bake();
 		pizza.cut();
