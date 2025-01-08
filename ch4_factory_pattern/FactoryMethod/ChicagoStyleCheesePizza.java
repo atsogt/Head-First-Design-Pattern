87ca4733a91e3f0bcd30290e5e7af021984ab382
atsogt <atsogt17@gmail.com>
2025-01-07 21:51:54 -0500

    cut method modifier changed to public

--- ch4_factory_pattern/FactoryMethod/ChicagoStyleCheesePizza.java
+++ ch4_factory_pattern/FactoryMethod/ChicagoStyleCheesePizza.java
@@ -10,7 +10,7 @@ public class ChicagoStyleCheesePizza extends Pizza {
 		toppings.add("Shredded Mozzarella Cheese");
 	}
 
-	void cut() {
+	public void cut() {
 		System.out.println("Cutting the pizza into square slices");
 	}
 }
